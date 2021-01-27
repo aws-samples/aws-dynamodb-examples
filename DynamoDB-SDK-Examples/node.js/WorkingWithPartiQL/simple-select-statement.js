6044b4824246cb84944fae0415df838e04276dca
Kirk Kirkconnell <kirkcon@amazon.com>
2021-01-27 10:20:54 -0800

    Updated to be more AWS JavaScript SDK v3 style

--- DynamoDB-SDK-Examples/node.js/WorkingWithPartiQL/simple-select-statement.js
+++ DynamoDB-SDK-Examples/node.js/WorkingWithPartiQL/simple-select-statement.js
@@ -1,25 +1,27 @@
-const AWS = require("aws-sdk");
-const converter = require('aws-sdk/lib/dynamodb/converter.js');
-AWS.config.update({ region: "us-west-2" });
+const REGION = "us-west-2";
+// I am using the DynamoDB low level because the DocClient does not support executeStatement used with PartiQL
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const { unmarshall } = require("@aws-sdk/util-dynamodb");
 
-const dynamodb = new AWS.DynamoDB();
+// Create low level client
+const dbclient = new DynamoDB({ region: REGION });
 
-async function executePartiQLStatement() {
-    // In PartiQL, the single quotes and double quotes matter. Double quotes are used to signify variable names, single quotes are for string literals.
-    // Notice the info.directors where I am getting the array of directors inside of the info map object.
+const executePartiQLStatement = async () => {
+    // In PartiQL, the single quotes and double quotes matter. Double quotes are used to signify variable names,
+    // single quotes are for string literals. Notice the info.directors where I am getting the array of directors
+    // inside of the info map object.
     const params = {
         Statement: `SELECT info.directors FROM Movies WHERE "year" = 2016 AND "title" = 'The Flash'`,
     }
-    const response = await dynamodb.executeStatement(params).promise();
-    return response;
+    return data = await dbclient.executeStatement(params);
 }
 
 executePartiQLStatement()
     .then((data) => {
             //Iterate through the Items array returned in the data variable and then unmarshall the DynamoDB JSON format to "regular" json format.
             data.Items.forEach(function (item) {
-                const doc = converter.unmarshall(item);
-                console.log(JSON.stringify(doc, null, 2));
+                //const doc = unmarshall(item);
+                console.log(JSON.stringify(unmarshall(item), null, 2));
             });
         }
     )
