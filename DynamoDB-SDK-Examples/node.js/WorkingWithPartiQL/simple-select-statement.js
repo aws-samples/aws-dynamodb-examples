5cdfbb60f0b6b9a05575e9dea26c6c38f3604ace
Kirk Kirkconnell <kirkcon@amazon.com>
2021-01-27 10:23:04 -0800

    Removed comment and fixed other comment

--- DynamoDB-SDK-Examples/node.js/WorkingWithPartiQL/simple-select-statement.js
+++ DynamoDB-SDK-Examples/node.js/WorkingWithPartiQL/simple-select-statement.js
@@ -8,8 +8,9 @@ const dbclient = new DynamoDB({ region: REGION });
 
 const executePartiQLStatement = async () => {
     // In PartiQL, the single quotes and double quotes matter. Double quotes are used to signify variable names,
-    // single quotes are for string literals. Notice the info.directors where I am getting the array of directors
-    // inside of the info map object.
+    // single quotes are for string literals.
+
+    // Notice the info.directors where I am getting the array of directors inside of the info map object.
     const params = {
         Statement: `SELECT info.directors FROM Movies WHERE "year" = 2016 AND "title" = 'The Flash'`,
     }
@@ -20,7 +21,6 @@ executePartiQLStatement()
     .then((data) => {
             //Iterate through the Items array returned in the data variable and then unmarshall the DynamoDB JSON format to "regular" json format.
             data.Items.forEach(function (item) {
-                //const doc = unmarshall(item);
                 console.log(JSON.stringify(unmarshall(item), null, 2));
             });
         }
