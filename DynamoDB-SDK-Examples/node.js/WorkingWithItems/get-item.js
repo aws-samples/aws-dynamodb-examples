36741038dddd4944b98c73f72fab0ac1e1fcf794
Kirk Kirkconnell <kirkcon@amazon.com>
2021-04-15 11:45:33 -0700

    Changing get-item to JS v3 SDK.

--- DynamoDB-SDK-Examples/node.js/WorkingWithItems/get-item.js
+++ DynamoDB-SDK-Examples/node.js/WorkingWithItems/get-item.js
@@ -1,24 +1,28 @@
-const AWS = require("aws-sdk");
+// This is an example of a simple GetItem with the higher level DocumentClient for Amazon DynamoDB
 
-AWS.config.update({ region: "us-west-2" });
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, GetCommand } = require("@aws-sdk/lib-dynamodb");
 
-const documentClient = new AWS.DynamoDB.DocumentClient();
-
-async function getItem() {
-  const params = {
-    TableName: "RetailDatabase",
-    Key: {
-      pk: "jim.bob@somewhere.com",
-      sk: "metadata",
-    },
-  };
-
-  const response = await documentClient.get(params).promise();
-  return response;
+async function getItems() {
+  const client = new DynamoDBClient({ region: "us-west-2" });
+  const ddbDocClient = DynamoDBDocumentClient.from(client);
+  try {
+    return await ddbDocClient.send(
+        new GetCommand({
+          TableName: "RetailDatabase",
+          Key: {
+            pk: "joe@somewhere.com",
+            sk: "metadata"
+          },
+        })
+    );
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-getItem()
-  .then((data) =>
-    console.log("GetItem succeeded:", JSON.stringify(data, null, 2))
-  )
-  .catch((error) => console.error(JSON.stringify(error, null, 2)));
+getItems()
+    .then((data) =>
+        console.log("GetItem succeeded:", JSON.stringify(data.Item, null, 2))
+    )
+    .catch((error) => console.error(JSON.stringify(error, null, 2)));
