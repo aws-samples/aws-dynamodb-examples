9464c0c8c5873051e2ccb573686d6fac414a580a
Kirk Kirkconnell <kirkcon@amazon.com>
2021-04-15 16:54:58 -0700

    Fixed the comments

--- DynamoDB-SDK-Examples/node.js/WorkingWithItems/put-item-conditional.js
+++ DynamoDB-SDK-Examples/node.js/WorkingWithItems/put-item-conditional.js
@@ -1,40 +1,5 @@
-/*const AWS = require("aws-sdk");
-
-AWS.config.update({ region: "us-west-2" });
-
-const documentClient = new AWS.DynamoDB.DocumentClient();
-
-// Define new user data to insert into the system. Notice the conditional expression where it will prevent overwriting if the item already exists
-async function putItem() {
-  const params = {
-    TableName: "RetailDatabase",
-    Item: {
-      pk: "jim.bob@somewhere.com",
-      sk: "metadata",
-      name: "Jim Bob",
-      first_name: "Jim",
-      last_name: "Bob",
-      address: {
-        road: "456 Nowhere Lane",
-        city: "Langely",
-        state: "WA",
-        pcode: "98260",
-        country: "USA",
-      },
-      username: "jbob",
-    },
-    ConditionExpression: "attribute_not_exists(sk)",
-  };
-
-  return await documentClient.put(params).promise();
-}
-
-putItem()
-  .then(() => console.log("PutItem succeeded"))
-  .catch((error) => console.error(JSON.stringify(error, null, 2)));
-*/
-
-// This is an example of a simple GetItem with the higher level DocumentClient for Amazon DynamoDB
+// This is an example of a simple PutItem with a ConditionExpression using
+// the higher level DocumentClient for Amazon DynamoDB
 
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
