265ab09d4a4e658290bffaed5d2d14feab411569
Kirk Kirkconnell <kirkcon@amazon.com>
2021-04-16 15:58:03 -0700

    Updated batch-write to v3

--- DynamoDB-SDK-Examples/node.js/WorkingWithItems/batch-write.js
+++ DynamoDB-SDK-Examples/node.js/WorkingWithItems/batch-write.js
@@ -1,62 +1,68 @@
-const AWS = require("aws-sdk");
+// This is an example of a simple BatchWriteCommand with the higher level DocumentClient for Amazon DynamoDB
 
-AWS.config.update({ region: "us-west-2" });
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, BatchWriteCommand } = require("@aws-sdk/lib-dynamodb");
 
-const documentClient = new AWS.DynamoDB.DocumentClient();
-
-// Batch write 2 items into the table
-async function batchWriteItem() {
-  const params = {
-    RequestItems: {
-      RetailDatabase: [
-        {
-          PutRequest: {
-            Item: {
-              pk: "vikram.johnson@somewhere.com",
-              sk: "metadata",
-              username: "vikj",
-              first_name: "Vikram",
-              last_name: "Johnson",
-              name: "Vikram Johnson",
-              age: 31,
-              address: {
-                road: "89105 Bakken Rd",
-                city: "Greenbank",
-                pcode: 98253,
-                state: "WA",
-                country: "USA",
+async function batchWriteItems() {
+  const client = new DynamoDBClient({ region: "us-west-2" });
+  const ddbDocClient = DynamoDBDocumentClient.from(client);
+  try {
+    return await ddbDocClient.send(
+        new BatchWriteCommand({
+          RequestItems: {
+            RetailDatabase: [
+              {
+                PutRequest: {
+                  Item: {
+                    pk: "billy.johnson@somewhere.com",
+                    sk: "metadata",
+                    username: "vikj",
+                    first_name: "Billy",
+                    last_name: "Johnson",
+                    name: "Billy Johnson",
+                    age: 31,
+                    address: {
+                      road: "89105 Bakken Rd",
+                      city: "Greenbank",
+                      pcode: 98253,
+                      state: "WA",
+                      country: "USA",
+                    },
+                  },
+                },
               },
-            },
-          },
-        },
-        {
-          PutRequest: {
-            Item: {
-              pk: "jose.schneller@somewhere.com",
-              sk: "metadata",
-              username: "joses",
-              first_name: "Jose",
-              last_name: "Schneller",
-              name: "Jose Schneller",
-              age: 27,
-              address: {
-                road: "12341 Fish Rd",
-                city: "Freeland",
-                pcode: 98249,
-                state: "WA",
-                country: "USA",
+              {
+                PutRequest: {
+                  Item: {
+                    pk: "juan.schneller@somewhere.com",
+                    sk: "metadata",
+                    username: "joses",
+                    first_name: "Juan",
+                    last_name: "Schneller",
+                    name: "Juan Schneller",
+                    age: 27,
+                    address: {
+                      road: "12341 Fish Rd",
+                      city: "Freeland",
+                      pcode: 98249,
+                      state: "WA",
+                      country: "USA",
+                    },
+                  },
+                },
               },
-            },
+            ],
           },
-        },
-      ],
-    },
-  };
-
-  const response = await documentClient.batchWrite(params).promise();
-  return response;
+          //This line returns in the response how much capacity the batch get uses
+          ReturnConsumedCapacity: "TOTAL",
+        })
+    );
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-batchWriteItem()
-  .then(() => console.log("BatchWriteItem succeeded"))
-  .catch((error) => console.error(JSON.stringify(error, null, 2)));
+batchWriteItems()
+    .then((data) =>
+        console.log("BatchWriteCommand succeeded:", JSON.stringify(data, null, 2)))
+    .catch((error) => console.error(JSON.stringify(error, null, 2)));
