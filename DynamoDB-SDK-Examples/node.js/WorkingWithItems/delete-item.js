1f8da9c1d7c0e81e06ca015e8f1385fd5d45eab1
Kirk Kirkconnell <kirkcon@amazon.com>
2021-04-16 11:20:24 -0700

    Changed from v2 to v3 style

--- DynamoDB-SDK-Examples/node.js/WorkingWithItems/delete-item.js
+++ DynamoDB-SDK-Examples/node.js/WorkingWithItems/delete-item.js
@@ -1,27 +1,32 @@
-const AWS = require("aws-sdk");
+/* This is an example of a Delete with a ConditionExpression using
+the higher level DocumentClient for Amazon DynamoDB. The conditional
+expression must be true for this DeleteCommand to succeed. */
 
-AWS.config.update({ region: "us-west-2" });
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, DeleteCommand } = require("@aws-sdk/lib-dynamodb");
 
-const documentClient = new AWS.DynamoDB.DocumentClient();
-
-// Notice the conditional expression where it will only delete if the postal code is the specified value
 async function deleteItem() {
-  const params = {
-    TableName: "RetailDatabase",
-    Key: {
-      pk: "jim.bob@somewhere.com",
-      sk: "metadata",
-    },
-    ConditionExpression: "address.pcode = :val",
-    ExpressionAttributeValues: {
-      ":val": "98260",
-    },
-  };
-
-  const response = await documentClient.delete(params).promise();
-  return response;
+  const client = new DynamoDBClient({ region: "us-west-2" });
+  const ddbDocClient = DynamoDBDocumentClient.from(client);
+  return await ddbDocClient.send(
+      new DeleteCommand({
+        TableName: "RetailDatabase",
+        Key: {
+          pk: "jim.bob@somewhere.com",    // Partition key
+          sk: "metadata",                 // Sort key
+        },
+        // Below is an optional conditional expression to validate the value of the address.pcode
+        // value is the value provided. If the item matching the partition and sort key are found
+        // but the values below does not match, then the delete will fail and the item not deleted.
+        ConditionExpression: "address.pcode = :val",
+        ExpressionAttributeValues: {
+          ":val" : "98261",
+        },
+      })
+  );
 }
 
 deleteItem()
-  .then(() => console.log("DeleteItem succeeded"))
-  .catch((error) => console.error(JSON.stringify(error, null, 2)));
+    .then((data) =>
+        console.log("PutItem succeeded:", JSON.stringify(data, null, 2)))
+    .catch((error) => console.error(JSON.stringify(error, null, 2)));
