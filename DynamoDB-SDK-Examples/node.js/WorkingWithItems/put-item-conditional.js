813ded95bc9072e96d042b5093f21e28da6d8b31
Kirk Kirkconnell <kirkcon@amazon.com>
2021-04-16 09:37:19 -0700

    Fixed the conditional expression and error handling

--- DynamoDB-SDK-Examples/node.js/WorkingWithItems/put-item-conditional.js
+++ DynamoDB-SDK-Examples/node.js/WorkingWithItems/put-item-conditional.js
@@ -1,5 +1,6 @@
-// This is an example of a simple PutItem with a ConditionExpression using
-// the higher level DocumentClient for Amazon DynamoDB
+/* This is an example of a simple PutItem with a ConditionExpression using
+the higher level DocumentClient for Amazon DynamoDB. The conditional
+expression must be true for this Put call to succeed. */
 
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
@@ -7,31 +8,30 @@ const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
 async function putItems() {
   const client = new DynamoDBClient({ region: "us-west-2" });
   const ddbDocClient = DynamoDBDocumentClient.from(client);
-  try {
-    return await ddbDocClient.send(
-        new PutCommand({
-          TableName: "RetailDatabase",
-          Item: {
-            pk: "jim.Robert@somewhere.com",
-            sk: "metadata",
-            name: "Jim Roberts",
-            first_name: "Jim",
-            last_name: "Roberts",
-            address: {
-              road: "456 Nowhere Lane",
-              city: "Langely",
-              state: "WA",
-              pcode: "98260",
-              country: "USA",
-            },
-            username: "jrob",
+  return await ddbDocClient.send(
+      new PutCommand({
+        TableName: "RetailDatabase",
+        Item: {
+          pk: "jim.Robert@somewhere.com", // Partition key
+          sk: "metadata",                 // Sort key
+          name: "Jim Roberts",
+          first_name: "Jim",
+          last_name: "Roberts",
+          address: {
+            road: "456 Nowhere Lane",
+            city: "Langely",
+            state: "WA",
+            pcode: "98260",
+            country: "USA",
           },
-          ConditionExpression: "attribute_not_exists(sk)",
-        })
-    );
-  } catch (err) {
-    console.error(err);
-  }
+          username: "jrob",
+        },
+        // Start conditional expression to make sure the value of the sk attribute begins with the string "meta".
+        // If not, then the put will fail with a "ConditionalCheckFailedException"
+        ConditionExpression: "begins_with(sk, :val)",
+        ExpressionAttributeValues: {":val" : "meta"}
+      })
+  );
 }
 
 putItems()
