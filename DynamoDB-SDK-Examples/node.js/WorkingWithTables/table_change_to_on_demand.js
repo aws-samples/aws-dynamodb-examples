68d53fe97228d73e24e5989751af28f1ebb2fe91
Kirk Kirkconnell <kirkcon@amazon.com>
2021-04-23 12:38:22 -0700

    Change to v3

--- DynamoDB-SDK-Examples/node.js/WorkingWithTables/table_change_to_on_demand.js
+++ DynamoDB-SDK-Examples/node.js/WorkingWithTables/table_change_to_on_demand.js
@@ -1,17 +1,21 @@
-const AWS = require("aws-sdk");
+// A simple script to switch a table to on-demand capacity mode.
 
-const dynamodb = new AWS.DynamoDB({ region: "us-west-2" });
+const { DynamoDBClient, UpdateTableCommand } = require('@aws-sdk/client-dynamodb');
 
-const updateTable = async () => {
-  const response = await dynamodb
-    .updateTable({
-      BillingMode: "PAY_PER_REQUEST",
-      TableName: "Music", // Substitute your table name for "Music"
-    })
-    .promise();
+const REGION = "us-west-2"; // Replace with your target region
+const TableName = "Music"; // Replace with your table name.
 
-  await dynamodb.waitFor("tableExists", { TableName: "Music" }).promise();
-  console.log("Table has been updated");
-};
+const dbclient = new DynamoDBClient({ region: REGION });
 
-updateTable().catch((error) => console.error(JSON.stringify(error, null, 2)));
+async function updateTable() {
+    const params = {
+        BillingMode: "PAY_PER_REQUEST",
+        TableName: TableName,
+    };
+    return await dbclient.send( new UpdateTableCommand(params));
+    // Add code to wait for table update to complete before returning.
+}
+
+updateTable()
+    .then((data) => console.log(data))
+    .catch((error) => console.log("An error occured while updating the table:" + ' ' + error.message ));
