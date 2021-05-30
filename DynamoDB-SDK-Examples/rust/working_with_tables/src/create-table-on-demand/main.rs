e7512cd8c27982092fe438a6843bed427e8d9a8d
Erben Mo <moerben@amazon.com>
2021-05-30 00:04:38 -0700

    Add ddb-rust-sdk examples for commont table operation like list, create, describe and delete

---
+++ DynamoDB-SDK-Examples/rust/working_with_tables/src/create-table-on-demand/main.rs
@@ -0,0 +1,39 @@
+use dynamodb::model::{AttributeDefinition, BillingMode, KeySchemaElement, KeyType,
+                      ScalarAttributeType};
+
+#[tokio::main]
+async fn main() -> Result<(), dynamodb::Error> {
+    let client = dynamodb::Client::from_env();
+
+    let table_name = String::from("Music");
+    let hash_key = String::from("Artist");
+    let range_key = String::from("SongTitle");
+
+    let hash_key_schema = KeySchemaElement::builder()
+        .attribute_name(&hash_key)
+        .key_type(KeyType::Hash)
+        .build();
+    let range_key_schema = KeySchemaElement::builder()
+        .attribute_name(&range_key)
+        .key_type(KeyType::Range)
+        .build();
+
+    let hash_key_attribute = AttributeDefinition::builder()
+        .attribute_name(String::from(&hash_key))
+        .attribute_type(ScalarAttributeType::S)
+        .build();
+    let range_key_attribute = AttributeDefinition::builder()
+        .attribute_name(String::from(&range_key))
+        .attribute_type(ScalarAttributeType::S)
+        .build();
+
+    let resp = client.create_table()
+        .table_name(table_name)
+        .set_key_schema(Some(vec![hash_key_schema, range_key_schema]))
+        .set_attribute_definitions(Some(vec![hash_key_attribute, range_key_attribute]))
+        .billing_mode(BillingMode::PayPerRequest)
+        .send()
+        .await?;
+    println!("Created DynamoDB table: {:?}", resp.table_description);
+    Ok(())
+}
