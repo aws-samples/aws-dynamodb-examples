31b8b074232be80bfcb2a05a6455b151a247d44d
Jones Zachariah Noel N <joneszachariahnoel@gmail.com>
2021-10-06 19:17:09 +0530

    SDK config update with region.

--- DynamoDB-SDK-Examples/node.js/WorkingWithIndexes/create-index.js
+++ DynamoDB-SDK-Examples/node.js/WorkingWithIndexes/create-index.js
@@ -1,32 +1,33 @@
 //Import AWS SDK and initialize DynamoDB with the region.
 const REGION = "us-west-2";
 const AWS = require('aws-sdk');
-const dbclient = new AWS.DynamoDB({ region: REGION });
+AWS.config.update({ region: REGION });
+const dbclient = new AWS.DynamoDB();
 
-const createIndex=async()=>{
+const createIndex = async () => {
     let params = {
         TableName: 'copa-america',
-        GlobalSecondaryIndexUpdates :{
-            Create :{
-                IndexName :"group_id-group_ranking-index",
-                KeySchema:[
+        GlobalSecondaryIndexUpdates: {
+            Create: {
+                IndexName: "group_id-group_ranking-index",
+                KeySchema: [
                     {
-                        AttributeName:"group_id",
-                        KeyType:"HASH"
+                        AttributeName: "group_id",
+                        KeyType: "HASH"
                     },
                     {
-                        AttributeName:"group_ranking",
-                        KeyType:"RANGE"
+                        AttributeName: "group_ranking",
+                        KeyType: "RANGE"
                     }
                 ],
-                Projection :{
-                    ProjectionType :"ALL"
+                Projection: {
+                    ProjectionType: "ALL"
                 }
             }
         }
     }
     //Creating Indexes is supported with the updateTable API which takes the parameters such as IndexName, Schema for index and the projection.
-    let response= await dbclient.updateTable(params).promise()
+    let response = await dbclient.updateTable(params).promise()
 }
 
 createIndex().catch((error) => console.error(JSON.stringify(error, null, 2)));
