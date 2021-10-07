b453a249676b69f0b4695c6b6d46c2b57d6f078d
Kirk Kirkconnell <kirkcon@amazon.com>
2021-10-07 16:13:33 -0700

    Fixed delete and created describe

--- DynamoDB-SDK-Examples/node.js/WorkingWithBackups/DescribeBackup.js
+++ DynamoDB-SDK-Examples/node.js/WorkingWithBackups/DescribeBackup.js
@@ -1,18 +1,19 @@
-// A simple script to delete an on-demand backup of a DynamoDB table.
+// A simple script to describe an on-demand backup of a DynamoDB table.
 
-const { DynamoDBClient, DeleteBackupCommand } = require('@aws-sdk/client-dynamodb'); // ES Modules import
+const { DynamoDBClient, DescribeBackupCommand } = require('@aws-sdk/client-dynamodb'); // ES Modules import
 
 const REGION = "us-west-2";
 const TableName = "<your-table-name>";
 const AccountNumber = "<your-aws-account-number>";
 const BackupID = "<your-backup-id>";
+
 const BackupArn = "arn:aws:dynamodb:" + REGION + ":" + AccountNumber + ":table/"+ TableName + "/backup/" + BackupID;
 
-async function deleteBackup() {
+async function DescribeBackup() {
     const client = new DynamoDBClient({ region: REGION });
     try {
         return await client.send(
-            new DeleteBackupCommand({
+            new DescribeBackupCommand({
                 BackupArn: BackupArn,
             },)
         );
@@ -21,6 +22,6 @@ async function deleteBackup() {
     }
 }
 
-deleteBackup()
+DescribeBackup()
     .then((data) => console.log(data))
     .catch((error) => console.log("An error occured while deleting a backup:" + ' ' + error.message ));
