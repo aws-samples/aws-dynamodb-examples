ca594be5018da67f97a2633dd1e403d6bedb97ed
Kirk Kirkconnell <kirkcon@amazon.com>
2021-10-07 12:14:09 -0700

    Create UpdateIndexProvisionedCapacity.js

---
+++ DynamoDB-SDK-Examples/node.js/WorkingWithIndexes/UpdateIndexProvisionedCapacity.js
@@ -0,0 +1,34 @@
+// A simple script to update the provisioned capacity of a global secondary index (GSI) on an existing table DynamoDB table.
+// Note: this specific script does not alter a GSI's auto-scaling settings or Contributor Insights settings. That is a different script.
+
+const { DynamoDBClient, UpdateTableCommand } = require('@aws-sdk/client-dynamodb'); // ES Modules import
+
+const REGION = "us-west-2";
+const TableName = "Music";
+const IndexName = "SongTitle-Artist-index";
+
+async function createIndex() {
+    const client = new DynamoDBClient({ region: REGION });
+    try {
+        return await client.send(
+            new UpdateTableCommand({
+                TableName: TableName,
+                GlobalSecondaryIndexUpdates: [ {
+                    Update: {
+                        IndexName: IndexName,
+                        ProvisionedThroughput: {
+                            ReadCapacityUnits: 25,
+                            WriteCapacityUnits: 25,
+                        },
+                    }
+                }],
+            },)
+        );
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+createIndex()
+    .then((data) => console.log(data.TableDescription))
+    .catch((error) => console.log("An error occured while creating the index:" + ' ' + error.message ));
