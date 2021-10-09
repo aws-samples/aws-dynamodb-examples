c64c4277817cecd76ea51401e611df12f3fb029b
Lee Hannigan <lhnng@amazon.com>
2021-10-09 15:49:37 +0100

    Updating Python, adding txGet, fixing txWrite

--- DynamoDB-SDK-Examples/python/WorkingWithItems/transact_write.py
+++ DynamoDB-SDK-Examples/python/WorkingWithItems/transact_write.py
@@ -1,92 +1,58 @@
 from __future__ import print_function # Python 2/3 compatibility
-import boto3, json, decimal
-from boto3.dynamodb.conditions import Key, Attr
+import boto3, json
 from botocore.exceptions import ClientError
 
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
-
-table = dynamodb.Table("RetailDatabase")
+client = boto3.client('dynamodb', region_name='us-west-2')
 
 try:
-    response = table.meta.client.transact_write_items(
-        RequestItems={
-            'RetailDatabase': {
-                'Keys': [
-                    {
-                        'pk': your_partition_key,
-                        'sk': your_sort_key
+    response = client.transact_write_items(
+        TransactItems=[
+            {
+                'Put': {
+                    'TableName': 'RetailDatabase',
+                    'Item': {
+                        'PK': {
+                            'S': '123'
+                        },
+                        'SK': {
+                            'S': 'abc'
+                        },
+                        'User': {
+                            'S': 'lhnng'
+                        },
+                        'Other': {
+                            'S': 'Other attribute field'
+                        },
                     },
-                    {
-                        'pk': your_partition_key2,
-                        'sk': your_sort_key2
+                    'ConditionExpression': 'attribute_not_exists(PK)',
+                }
+            },
+            {
+                'Update': {
+                    'TableName': 'RetailDatabase',
+                    'Key': {
+                        'PK': {
+                            'S': '789'
+                        },
+                        'SK': {
+                            'S': 'xyz'
+                        }
                     },
-                ],
-                'ConsistentRead': False # For my user case, this data it is not changed often so why not get the reads at half price? Your use case might be different and need True.
+                    'UpdateExpression': 'SET #o = :val',
+                    'ExpressionAttributeValues': {
+                        ':val': {
+                            'S': 'Hello, updated value'
+                        }
+                    },
+                    'ExpressionAttributeNames': {
+                        '#o': 'Other'
+                    }
+                }
             }
-        },
-        ReturnConsumedCapacity='TOTAL'
+        ]
     )
 except ClientError as e:
+    print(e)
     print(e.response['Error']['Message'])
 else:
-    item = response['Responses']
-    print("BatchGetItem succeeded:")
-    print(json.dumps(item, indent=4, cls=DecimalEncoder))
-
-
-
-with table.batch_writer() as batch:
-    batch.put_item(
-        Item={
-            'pk': 'vikram.johnson@somewhere.com',
-            'sk': 'metadata',
-            'username': 'vikj',
-            'first_name': 'Vikram',
-            'last_name': 'Johnson',
-            'name': 'Vikram Johnson',
-            'age': 31,
-            'address': {
-                'road': '910 Bakken Rd',
-                'city': 'Greenbank',
-                'pcode': 98253,
-                'state': 'WA',
-                'country': 'USA'
-            }
-        }
-    )
-    batch.put_item(
-        Item={
-            'pk': 'jose.schneller@somewhere.com',
-            'sk': 'metadata',
-            'username': 'joses',
-            'first_name': 'Jose',
-            'last_name': 'Schneller',
-            'name': 'Jose Schneller',
-            'age': 27,
-            'address': {
-                'road': '1234 Fish Rd',
-                'city': 'Freeland',
-                'pcode': 98249,
-                'state': 'WA',
-                'country': 'USA'
-            }
-        }
-    )
-    batch.put_item(
-        Item={
-            'pk': 'helga.ramirez@somewhere.com',
-            'sk': 'metadata',
-            'username': 'helgar',
-            'first_name': 'Helga',
-            'last_name': 'Ramirez',
-            'name': 'Helga Ramirez',
-            'age': 48,
-            'address': {
-                'road': '5678 Deer Lake Rd',
-                'city': 'Clinton',
-                'pcode': 98236,
-                'state': 'WA',
-                'country': 'USA'
-            }
-        }
-    )
+    print("Transact Write succeeded")
