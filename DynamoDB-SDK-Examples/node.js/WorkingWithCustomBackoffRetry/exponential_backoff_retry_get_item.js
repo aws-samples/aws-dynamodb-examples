b78d16adf1949fc84ee3c4bdf6aa536efd9e81cc
Manoj Pathak <manojpathak227@gmail.com>
2021-11-11 18:06:01 -0600

    Feature: Add changes for nodejs example for custom exponential back off and retry

---
+++ DynamoDB-SDK-Examples/node.js/WorkingWithCustomBackoffRetry/exponential_backoff_retry_get_item.js
@@ -0,0 +1,51 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, GetCommand } = require("@aws-sdk/lib-dynamodb");
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+async function getItems() {
+    let retries = 0;
+    let condition = true;
+    const MAX_RETRY = 7;
+    const client = new DynamoDBClient({ region: "us-east-1", maxRetries: 0 });
+    const ddbDocClient = DynamoDBDocumentClient.from(client);
+    while (condition) {
+        try {
+            condition = false;
+            return await ddbDocClient.send(
+                new GetCommand({
+                    TableName: "Music",
+                    Key: {
+                        artist: "batman0", // Partition Key
+                        title: "happy0"          // Sort Key
+                    },
+                    ConsistentRead: false,
+                })
+            );
+        } catch (err) {
+            retries += 1;
+            backoffTime = (2 ** retries);
+            if (retries < MAX_RETRY) {
+                return delay(backoffTime);
+            }
+            else {
+                condition = False;
+                console.log("Max retry Done or not retriable");
+                throw err;
+            }
+        }
+    }
+}
+
+getItems()
+    .then((data) =>
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2)))
+    .catch((error) => console.error(JSON.stringify(error, null, 2)));
+
+
+
+
+
+
+
+
+
