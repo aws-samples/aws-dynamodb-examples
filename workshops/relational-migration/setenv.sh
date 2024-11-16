47a43bedf75bc0859e9141ad1bdd1f330f0933f1
Rob McCauley <robm26@gmail.com>
2024-11-15 19:56:03 -0500

    updates to environment scripts to use sed

--- workshops/relational-migration/setenv.sh
+++ workshops/relational-migration/setenv.sh
@@ -31,17 +31,17 @@ if [[ "workshop" == $DEPLOYMENT ]]
     SECURITY_GROUP=$(aws ec2 describe-instances  --query 'Reservations[0].Instances[0].SecurityGroups[0].GroupId' --filters Name=tag:Name,Values=MySQL-Instance)
     export SECURITY_GROUP=$(echo $SECURITY_GROUP | xargs)
 
-    jq_update='.stages.relational.subnet_ids=["'
-    jq_update+=$SUBNET_ID
-    jq_update+='"]'
-
-    jq $jq_update .chalice/config.json > "$tmp" && mv "$tmp" .chalice/config.json
-
-    jq_update='.stages.relational.security_group_ids=["'
-    jq_update+=$SECURITY_GROUP
-    jq_update+='"]'
-
-    jq $jq_update .chalice/config.json > "$tmp" && mv "$tmp" .chalice/config.json
+#    jq_update='.stages.relational.subnet_ids=["'
+#    jq_update+=$SUBNET_ID
+#    jq_update+='"]'
+#
+#    jq $jq_update .chalice/config.json > "$tmp" && mv "$tmp" .chalice/config.json
+#
+#    jq_update='.stages.relational.security_group_ids=["'
+#    jq_update+=$SECURITY_GROUP
+#    jq_update+='"]'
+#
+#    jq $jq_update .chalice/config.json > "$tmp" && mv "$tmp" .chalice/config.json
 
 
   else
@@ -61,13 +61,23 @@ echo MYSQL_USERNAME   = $MYSQL_USERNAME
 echo MYSQL_PASSWORD   = $MYSQL_PASSWORD
 echo MIGRATION_STAGE  = $MIGRATION_STAGE
 echo MIGRATION_BUCKET = $MIGRATION_BUCKET
+echo SECURITY_GROUP   = $SECURITY_GROUP
+echo SUBNET_ID        = $SUBNET_ID
+
 echo
 tmp=$(mktemp)
 
-jq_update='.stages.relational.environment_variables.MYSQL_HOST="'
-jq_update+=$MYSQL_HOST
-jq_update+='"'
+#jq_update='.stages.relational.environment_variables.MYSQL_HOST="'
+#jq_update+=$MYSQL_HOST
+#jq_update+='"'
+#
+#jq $jq_update .chalice/config.json > "$tmp" && mv "$tmp" .chalice/config.json
 
-jq $jq_update .chalice/config.json > "$tmp" && mv "$tmp" .chalice/config.json
+sed -i "s/##DBHOST##/${MYSQL_HOST}/g" .chalice/config.json
+sed -i "s/##DBUSERNAME##/${MYSQL_USERNAME}/g" .chalice/config.json
+sed -i "s/##DBPASSWORD##/${MYSQL_PASSWORD}/g" .chalice/config.json
+sed -i "s/##SUBNETID##/${SUBNET_ID}/g" .chalice/config.json
+sed -i "s/##SECURITYGROUPID##/${SECURITY_GROUP}/g" .chalice/config.json
+
 
 echo Chalice configuration file updated with MYSQL_HOST=$MYSQL_HOST
